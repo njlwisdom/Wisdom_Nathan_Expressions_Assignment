9f87cb7070bf4445309907186eee5e03ef6f86d1
njlwisdom <njlwisdom@fullsail.edu>
2015-03-12 17:17:57 -0500

    Initial Setup

--- Expressions Assignment/Expressions/js/script.js
+++ Expressions Assignment/Expressions/js/script.js
@@ -2,3 +2,17 @@
 //  SDI 1503
 //  Expressions Project
 
+// Add alert so that I know it works
+//alert("You are doing good!");
+
+//  Variables
+//  My rate of pay per hour
+var myPay                           = 12.93;
+//  How many hours I work per week
+var hourWeek;
+//  My place of employment
+var myJob                           = "Tyson Foods Inc.";
+
+//  Prompts
+hourweek = prompt ("Please enter how many hours you worked this week.");
+
