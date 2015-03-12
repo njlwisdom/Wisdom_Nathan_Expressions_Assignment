97755ab5906c0e2583f593958be22a2f5d2cda24
njlwisdom <njlwisdom@fullsail.edu>
2015-03-12 18:45:33 -0500

    Finished!

--- Expressions Assignment/Expressions/js/script.js
+++ Expressions Assignment/Expressions/js/script.js
@@ -1,5 +1,5 @@
 //  Nathan Wisdom
-//  SDI 1503
+//  3-12-15
 //  Expressions Project
 
 // Add alert so that I know it works
@@ -13,15 +13,19 @@ var hourWeek;
 //  My place of employment
 var myJob                           = "Tyson Foods Inc.";
 myPay = prompt("Please enter your hourly rate of pay.")
+// hours you worked this week
 hourWeek = prompt("Please enter how many hours you worked this week.");
 var weekPay = hourWeek * myPay
 
 console.log("Your weekly pay should be  " + weekPay + "");
 
 //  Variables
+// my weekly pay based on hours worked and my pay per hour
 var weekPay                         = 40 * 12.93
+// prompt for yearly bonus check amount
 var bonusCheck;
 bonusCheck = prompt("How much is your yearly bonus check at work?")
+// total check with weekly pay and bonus added together
 var totalCheck                      = weekPay + bonusCheck
     console.log("Every year at " + myJob + " I get a yearly bonus. This year I got my weekly pay check of " + weekPay + " with a " + bonusCheck + "  bonus to make my check " + totalCheck +"")
 
